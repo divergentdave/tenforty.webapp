932705fecd5fc04971d7c749536002fe5c932484
David Cook <divergentdave@gmail.com>
2016-12-28 13:42:08 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject tenforty.webapp "0.1.0-SNAPSHOT"
+  :description "Webapp for analyzing U.S. taxes"
+  :url "https://github.com/divergentdave/tenforty.webapp"
+  :license {:name "GNU GPL v2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [tenforty "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+
+  :cljsbuild {:builds {:dev {:source-paths ["src-cljs"]
+                             :compiler {:output-to "resources/public/js/cljs.js"
+                                        :output-dir "resources/public/js"
+                                        :optimizations :none
+                                        :pretty-print true
+                                        :source-map true}}
+                       :prod {:source-paths ["src-cljs"]
+                              :compiler {:output-to "resources/public/js-min/cljs-min.js"
+                                         :output-dir "resources/public/js-min"
+                                         :optimizations :advanced
+                                         :pretty-print false
+                                         :source-map "resources/public/js-min/cljs-min.js.map"}}}})
---
+++ src-cljs/tenforty/webapp/core.cljs
@@ -0,0 +1,6 @@
+(ns tenforty.webapp.core
+  (:require [tenforty.core :refer [calculate]]
+            [tenforty.forms.ty2015.f1040 :refer [form]]))
+
+(.write js/document "Hello!")
+
