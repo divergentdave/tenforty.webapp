966c0c37bbb09a90059bc89cf8b6435fe9fb034e
David Cook <divergentdave@gmail.com>
2016-12-31 17:13:58 -0600

    Draw graph of lines with dependencies

---
+++ resources/public/js/externs.js
@@ -0,0 +1,15 @@
+var d3 = {};
+d3.select = function() {};
+d3.select.prototype.attr = function() {};
+d3.select.prototype.append = function() {};
+d3.select.prototype.append.prototype.attr = function() {};
+
+var dagreD3 = {};
+dagreD3.graphlib = {};
+dagreD3.graphlib.Graph = function() {};
+dagreD3.graphlib.Graph.prototype.setGraph = function() {};
+dagreD3.graphlib.Graph.prototype.setDefaultEdgeLabel = function() {};
+dagreD3.graphlib.Graph.prototype.setNode = function() {};
+dagreD3.graphlib.Graph.prototype.setEdge = function() {};
+dagreD3.graphlib.Graph.prototype.graph = function() {};
+dagreD3.render = function () {};
--- src-cljs/tenforty/webapp/core.cljs
+++ src-cljs/tenforty/webapp/core.cljs
@@ -1,6 +1,27 @@
 (ns tenforty.webapp.core
-  (:require [tenforty.core :refer [calculate]]
-            [tenforty.forms.ty2015.f1040 :refer [form]]))
+  (:require [tenforty.core :refer [calculate
+                                   get-deps
+                                   get-name]]
+            [tenforty.forms.ty2015 :refer [forms]]))
 
-(.write js/document "Hello!")
+(defn init
+  []
+  (let [g (js/dagreD3.graphlib.Graph.)
+        svg (.select js/d3 "svg")
+        svg-group (.append svg "g")
+        render (js/dagreD3.render.)]
+    (.setGraph g {})
+    (.setDefaultEdgeLabel g #(js-obj))
+    (dorun (map #(.setNode g (get-name %) (js-obj "label" (get-name %))) (vals (:lines forms))))
+    (dorun (map (fn [dest] (dorun (map (fn [src] (.setEdge g (name src) (get-name dest)))
+                                       (get-deps dest))))
+                (vals (:lines forms))))
+    (render (.select js/d3 "svg g") g)
+    (let [graph (.graph g)
+          width (aget graph "width")
+          height (aget graph "height")]
+      (.attr svg "width" (+ width 40))
+      (.attr svg "height" (+ height 40)))
+    (.attr svg-group "transform" "translate(20, 20)")))
 
+(.addEventListener js/document "DOMContentLoaded" init)
