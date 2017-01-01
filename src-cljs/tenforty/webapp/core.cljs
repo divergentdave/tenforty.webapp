a44a80ababf8c5c1622de37ff22b39b1c550948a
David Cook <divergentdave@gmail.com>
2017-01-01 15:28:11 -0600

    Add color to input cells

--- src-cljs/tenforty/webapp/core.cljs
+++ src-cljs/tenforty/webapp/core.cljs
@@ -2,27 +2,42 @@
   (:require [tenforty.core :refer [calculate
                                    get-deps
                                    get-keyword
-                                   get-name]]
+                                   get-name
+                                   ->MapTaxSituation
+                                   input-line?]]
             [tenforty.forms.ty2015 :refer [forms]]))
 
-(defn init
-  []
-  (let [g (js/dagreD3.graphlib.Graph.)
-        svg (.select js/d3 "svg")
-        svg-group (.append svg "g")
-        render (js/dagreD3.render.)]
-    (.setGraph g {})
-    (.setDefaultEdgeLabel g #(js-obj))
-    (dorun (map #(.setNode g (str (get-keyword %)) (js-obj "label" (get-name %))) (vals (:lines forms))))
-    (dorun (map (fn [dest] (dorun (map (fn [src] (.setEdge g (str src) (str (get-keyword dest))))
-                                       (get-deps dest))))
-                (vals (:lines forms))))
-    (render (.select js/d3 "svg g") g)
-    (let [graph (.graph g)
-          width (aget graph "width")
-          height (aget graph "height")]
-      (.attr svg "width" (+ width 40))
-      (.attr svg "height" (+ height 40)))
-    (.attr svg-group "transform" "translate(20, 20)")))
+(let [situation (atom (->MapTaxSituation {} {}))]
+  (defn init
+    []
+    (let [g (js/dagreD3.graphlib.Graph.)
+          svg (.select js/d3 "svg")
+          svg-group (.append svg "g")
+          render (js/dagreD3.render.)]
+      (.setGraph g {})
+      (.setDefaultEdgeLabel g #(js-obj))
+      (dorun (map #(.setNode
+                    g
+                    (str (get-keyword %))
+                    (if (input-line? %)
+                      (js-obj "label" (get-name %)
+                              "class" "input empty-input")
+                      (js-obj "label" (get-name %))))
+                  (vals (:lines forms))))
+      (dorun (map
+              (fn [dest] (dorun (map
+                                 (fn [src] (.setEdge
+                                            g
+                                            (str src)
+                                            (str (get-keyword dest))))
+                                 (get-deps dest))))
+              (vals (:lines forms))))
+      (render (.select js/d3 "svg g") g)
+      (let [graph (.graph g)
+            width (aget graph "width")
+            height (aget graph "height")]
+        (.attr svg "width" (+ width 40))
+        (.attr svg "height" (+ height 40)))
+      (.attr svg-group "transform" "translate(20, 20)"))))
 
 (.addEventListener js/document "DOMContentLoaded" init)
