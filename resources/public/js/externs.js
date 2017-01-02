ed4503437345189ef9818385607f9bd727b3e0d1
David Cook <divergentdave@gmail.com>
2017-01-02 17:33:02 -0600

    Decorate graph nodes with input elements

--- resources/public/js/externs.js
+++ resources/public/js/externs.js
@@ -1,6 +1,8 @@
 var d3 = {};
 d3.select = function() {};
 d3.select.prototype.attr = function() {};
+d3.select.prototype.insert = function() {};
+d3.select.prototype.insert.prototype.attr = function() {};
 d3.select.prototype.append = function() {};
 d3.select.prototype.append.prototype.attr = function() {};
 
@@ -12,4 +14,6 @@ dagreD3.graphlib.Graph.prototype.setDefaultEdgeLabel = function() {};
 dagreD3.graphlib.Graph.prototype.setNode = function() {};
 dagreD3.graphlib.Graph.prototype.setEdge = function() {};
 dagreD3.graphlib.Graph.prototype.graph = function() {};
+dagreD3.intersect.rect = function() {};
 dagreD3.render = function () {};
+dagreD3.render.prototype.shapes = function() {};
--- src-cljs/tenforty/webapp/core.cljs
+++ src-cljs/tenforty/webapp/core.cljs
@@ -4,8 +4,70 @@
                                    get-keyword
                                    get-name
                                    ->MapTaxSituation
-                                   input-line?]]
+                                   NumberInputLine
+                                   BooleanInputLine
+                                   CodeInputLine]]
             [tenforty.forms.ty2015 :refer [forms]]))
+
+(defn- make-rect-intersect-callback [node]
+  (fn [point] (.rect js/dagreD3.intersect node point)))
+
+(defn- shape-helper [controls-fn]
+  (fn [parent bbox node]
+    (let [shape-svg (.insert parent "g" ":first-child")
+          rect (.append shape-svg "rect")
+          foreign-object (.append shape-svg "foreignObject")
+          body (.append foreign-object "xhtml:body")
+          div (.append body "div")]
+      (.attr div "class" "controls")
+      (let [bbox-width (aget bbox "width")
+            bbox-height (aget bbox "height")
+            width (max bbox-width 150)
+            height (* 2 bbox-height)]
+        (controls-fn div)
+        (.attr rect "x" (- (/ width 2)))
+        (.attr rect "y" (- (/ height 2)))
+        (.attr rect "width" width)
+        (.attr rect "height" height)
+        (.attr foreign-object "x" (- (/ width 2)))
+        (.attr foreign-object "y" 0)
+        (.attr foreign-object "width" width)
+        (.attr foreign-object "height" bbox-height)
+        (.attr (.select parent ".label") "transform" (str "translate(0," (- (/ bbox-height 2)) ")")))
+      (aset node "intersect" (make-rect-intersect-callback node))
+      shape-svg)))
+
+(defn- boolean-controls [div]
+  (let [label-true (.append div "label")
+        radio-true (.append label-true "input")
+        label-false (.append div "label")
+        radio-false (.append label-false "input")]
+    (.attr radio-true "type" "radio")
+    (.appendChild (.node label-true) (.createTextNode js/document "true"))
+    (.attr radio-false "type" "radio")
+    (.appendChild (.node label-false) (.createTextNode js/document "false"))))
+
+(defn- number-controls [div]
+  (let [input (.append div "input")]
+    (.attr input "type" "number")))
+
+(defn- enum-controls [div]
+  (let [input (.append div "select")]
+    (dorun (map
+            #(let [option (.append input "option")]
+               (.attr option "value" (val %))
+               (.text option (key %)))
+            (seq {"foo" 1 "bar" 2})))))
+
+(defn register-shapes
+  [render]
+  (let [shapes (.shapes render)]
+    (aset shapes "rectRadioButtons"
+          (shape-helper boolean-controls))
+    (aset shapes "rectInput"
+          (shape-helper number-controls))
+    (aset shapes "rectSelect"
+          (shape-helper enum-controls))))
 
 (let [situation (atom (->MapTaxSituation {} {}))]
   (defn init
@@ -14,13 +76,24 @@
           svg (.select js/d3 "svg")
           svg-group (.append svg "g")
           render (js/dagreD3.render.)]
+      (register-shapes render)
       (.setGraph g {})
       (.setDefaultEdgeLabel g #(js-obj))
       (dorun (map #(.setNode
                     g
                     (str (get-keyword %))
-                    (if (input-line? %)
+                    (condp instance? %
+                      NumberInputLine
                       (js-obj "label" (get-name %)
+                              "shape" "rectInput"
+                              "class" "input empty-input")
+                      BooleanInputLine
+                      (js-obj "label" (get-name %)
+                              "shape" "rectRadioButtons"
+                              "class" "input empty-input")
+                      CodeInputLine
+                      (js-obj "label" (get-name %)
+                              "shape" "rectSelect"
                               "class" "input empty-input")
                       (js-obj "label" (get-name %))))
                   (vals (:lines forms))))
